ff081cae944f6b3610d479a40e15411f78317928
Luiz <luizfranzon05@hotmail.com>
2025-02-08 09:18:32 -0300

    add: improved game with functions

--- app/src/main/java/com/luizfranzon/kotlinbasics/rockpaperscissors/RockPaperScissors.kt
+++ app/src/main/java/com/luizfranzon/kotlinbasics/rockpaperscissors/RockPaperScissors.kt
@@ -1,19 +1,17 @@
 package com.luizfranzon.kotlinbasics.rockpaperscissors
 
 fun main() {
-    println("Choose 'Rock', 'Paper' or 'Scissors': ")
-    val playerChoice = readln().lowercase()
-    val randomNumber: Int = (1..3).random()
 
-    val computerChoice = when (randomNumber) {
-        1 -> "rock"
-        2 -> "paper"
-        3 -> "scissors"
-        else -> "0"
+    while (true) {
+        val playerChoice = getUserChoice()
+
+        if (playerChoice == "leave") break
+
+        val computerChoice = getComputerChoice()
+
+        val gameResult = runGame(computerChoice, playerChoice)
+        println(gameResult)
     }
-
-    val gameResult = runGame(computerChoice, playerChoice)
-    println(gameResult)
 }
 
 fun runGame(computerChoice: String, playerChoice: String): String {
@@ -23,9 +21,38 @@ fun runGame(computerChoice: String, playerChoice: String): String {
         "paper" to "rock"
     )
 
-    println("Computer chose $computerChoice")
+    println("--- Computer chose $computerChoice")
 
-    return if (computerChoice == playerChoice) "Draw!"
-    else if (gameTable[computerChoice] == playerChoice) "Computer won!"
-    else "You won!"
+    return if (computerChoice == playerChoice) "\n- Draw!\n"
+    else if (gameTable[computerChoice] == playerChoice) "\n- Computer won!\n"
+    else "\n- You won!\n"
+}
+
+fun getUserChoice(): String {
+    println("Choose 'Rock', 'Paper' or 'Scissors' (or 'leave' to close the game): ")
+    val userInput = readln().lowercase()
+
+    return validateUserInput(userInput)
+}
+
+fun validateUserInput(userInput: String): String {
+    if (userInput != "rock" && userInput != "paper" && userInput != "scissors") {
+        println("Invalid choice!")
+        return getUserChoice()
+    }
+
+    return userInput
+}
+
+fun getComputerChoice(): String {
+    val randomNumber: Int = (1..3).random()
+
+    val computerChoice = when (randomNumber) {
+        1 -> "rock"
+        2 -> "paper"
+        3 -> "scissors"
+        else -> "0"
+    }
+
+    return computerChoice
 }
